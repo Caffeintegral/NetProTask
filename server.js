4ec1a5d9e44b8fbb874180edd4583c3c0923b4cc
satsuki13579 <kazusaichinomiya2220@gmail.com>
2018-12-22 04:45:10 +0900

    ターン機能もどき追加

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ var http = require("http");
 var socketIO = require("socket.io");
 var fs = require("fs");
 var admin = require('firebase-admin');
-var serviceAccount = require('../FirebaseAdminSDK_netPro.json');
+var serviceAccount = require('../FirebaseAdminSDK_netPro.json'); //秘密鍵　抜かれたらやばいやつ
 
+//データベースの定義
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://netpro-3c4a0.firebaseio.com/" //データベースのURL
 });
 
 var db = admin.database();
-var ref = db.ref('messages/room1/');
+var messageRef = db.ref('messages/room1/'); //データベースの階層
+var usersRef = db.ref('users/room1/'); //データベースの階層
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/html" });
@@ -23,10 +25,15 @@ server.listen(3000);
 // socket.IOを用いたリアルタイムWebを実装します。
 var io = socketIO.listen(server);
 var userHash = {};
+var userSet = {};
+var userList = [];
+var count = 0;
 
 // 接続されたら、connected!とコンソールにメッセージを表示します。
 io.sockets.on("connection", function (socket) {
-  console.log("connected");
+  // console.log("connected");
+
+
 
   // 描画情報がクライアントから渡されたら、接続中の他ユーザーへ
   // broadcastで描画情報を送ります。
@@ -51,24 +58,56 @@ io.sockets.on("connection", function (socket) {
     socket.broadcast.emit("lineWidth", width);
   });
 
+
+  //接続したとき（入室したとき）
   socket.on("connected", function (sender) {
     var msg = sender + "さんが入室しました";
-    userHash[socket.id] = sender; console.log(msg);
+    userHash[socket.id] = sender;
+    userSet[count] = sender;
+    usersRef.child(count).set({
+      name: sender,
+      isNowPlayer: false
+    });
+    userList.push(sender);
+    console.log(userSet);
+    console.log(msg);
+    console.log(userList);
+    count++;
   });
-
+  //リロードとかページから離れた時
   socket.on("disconnect", function () {
     if (userHash[socket.id]) {
       var msg = userHash[socket.id] + "さんが退出しました";
-      ref.push({
+      messageRef.push({
         sender: userHash[socket.id],
         message: msg
       });
+
+      var senderValue = Object.keys(userSet).filter((key) => {
+        return userSet[key] === userHash[socket.id]
+      });
+
+      console.log(senderValue);
+      db.ref('users/room1/' + senderValue + '/').remove();
+
+
+      userList.pop();
+      usersRef.child(userHash[socket.id]).remove();
       delete userHash[socket.id];
-      io.sockets.emit("publish", { value: msg });
+      // io.sockets.emit("publish", { value: msg });
+      console.log(userList);
       console.log(msg);
     }
 
-  });
-});
+    if (userList.length == 0) {
+      usersRef.set({
+        0: "",
+      });
+      count = 0;
+      db.ref('nowPlayer/room1').set({ name: '未定' });
+    }
 
+  });
+
+});
 
